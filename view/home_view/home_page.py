5c650efb705f690a293a6bf919f0234cab988f21
arya <xxb9611@outlook.com>
2024-01-24 22:36:36 +0800

    [add] init

--- view/home_view/home_page.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-
-class HomePage(ft.UserControl):
-    def __init__(self):
-        super().__init__()
-
-    def build(self):
-        return ft.Text("Hello World")
--- view/set_view/set_page.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-
-class SetPage(ft.UserControl):
-    def __init__(self):
-        super().__init__()
-
-    def build(self):
-        return ft.Text("Hello set")
